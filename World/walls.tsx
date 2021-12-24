bde7338cc1dcdef335ac0d4a07f3420dfc55109d
Mike Tewner <mike@playstudios-il.com>
2021-12-24 14:59:31 +0200

    Add placeholder tilesetcopyright.
    TODO: Update copyrights

--- World/walls.tsx
+++ World/walls.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="wall" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <properties>
+  <property name="tilesetCopyright" value="unknown"/>
+ </properties>
  <image source="../Chaospatinnen_Tileset/wall.png" width="32" height="32"/>
  <tile id="0">
   <properties>
--- misc.tsx
+++ misc.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="misc" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
+ <properties>
+  <property name="tilesetCopyright" value="unknown"/>
+ </properties>
  <image source="Other_Tilesets/misc.png" width="512" height="2048"/>
 </tileset>
