74ec0a41e281d8962d44c066ef5663afed18a964
Mike Tewner <mike@playstudios-il.com>
2021-12-24 15:48:55 +0200

    Add default tilesetCopyright - again?

--- Chaosmentoren_Tileset_solid.tsx
+++ Chaosmentoren_Tileset_solid.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Chaosmentoren_Tileset_solid" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+<tileset version="1.5" tiledversion="1.7.2" name="Chaosmentoren_Tileset_solid" tilewidth="32" tileheight="32" tilecount="0" columns="32">
+ <properties>
+  <property name="tilesetCopyright" value="unknown"/>
+ </properties>
  <image source="../Chaospatinnen_Tileset/Chaosmentoren_Tileset.png" width="1024" height="1024"/>
 </tileset>
